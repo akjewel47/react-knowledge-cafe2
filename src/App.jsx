98dc66b940b58d13aa84b3764816fa8d94090ebf
Md jewel <jewel3472311@gmail.com>
2024-06-20 22:05:27 +0600

    added the json file and blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
 
@@ -6,8 +8,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-6xl bg-red-300'>Knowledge cafe</h1>
-   
+      <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
